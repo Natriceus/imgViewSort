c780c3ee37cf9318bbd6655f9f573820026b50e1
Natriceus <natriceus@gmail.com>
2014-05-06 16:54:47 +0200

    Added support for spaces in filenames.

--- imgViewSort.cpp
+++ imgViewSort.cpp
@@ -27,14 +27,14 @@ int main()
 
             cout << file;
 
-            //system((command0 + file).c_str());
+            system((command0 + " '" + file + "'").c_str());
             cout <<"choices: 1, 2, 3." << "\n";
             cin >> a;
             switch(a)
             {
-                case 1: system((command1 + file).c_str());//(combined string) to char, as required by command()
-                case 2: system((command2 + file + destination1).c_str());
-                case 3: system((command2 + file + destination2).c_str());
+                case 1: system((command1 + " '" + file + "'").c_str());//(combined string) to char, as required by command()
+                case 2: system((command2 + " '" + file + "' '" + destination1 + "'").c_str());
+                case 3: system((command2 + " '" + file + "' '" + destination2 + "'" ).c_str());
                 default: cout << "1: delete \n2:mv1\n3:mv2 \n";
 
             }
