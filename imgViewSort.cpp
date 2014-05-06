9c378d9fdd9170479c1ede984b51c71d304d9209
Natriceus <natriceus@gmail.com>
2014-05-06 16:49:22 +0200

    Only try to open jpg or png.

--- imgViewSort.cpp
+++ imgViewSort.cpp
@@ -23,18 +23,21 @@ int main()
     while (1){
         dirent* b = readdir(currentdir);
         string file = b[0].d_name;
-        cout << file;
+        if (file.find(".jpg") != string::npos | file.find(".png") != string::npos) {
 
-        //system((command0 + file).c_str());
-        cout <<"choices: 1, 2, 3." << "\n";
-        cin >> a;
-        switch(a)
-        {
-            case 1: system((command1 + file).c_str());//(combined string) to char, as required by command()
-            case 2: system((command2 + file + destination1).c_str());
-            case 3: system((command2 + file + destination2).c_str());
-            default: cout << "1: delete \n2:mv1\n3:mv2 \n";
+            cout << file;
 
+            //system((command0 + file).c_str());
+            cout <<"choices: 1, 2, 3." << "\n";
+            cin >> a;
+            switch(a)
+            {
+                case 1: system((command1 + file).c_str());//(combined string) to char, as required by command()
+                case 2: system((command2 + file + destination1).c_str());
+                case 3: system((command2 + file + destination2).c_str());
+                default: cout << "1: delete \n2:mv1\n3:mv2 \n";
+
+            }
         }
     }
 }
