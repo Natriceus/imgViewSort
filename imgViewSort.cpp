f7465583a0889202ffbbf344c85040507339ef84
Natriceus <natriceus@gmail.com>
2014-05-06 16:56:39 +0200

    Directories can now be chosen, updated help text.

--- imgViewSort.cpp
+++ imgViewSort.cpp
@@ -14,29 +14,36 @@ int main()
     string command0 = "feh ";
     string command1 = "rm -r ";
     string command2 = "mv ";
+    string help = "1: Delete file \n2: mv to destination 1 \n3: mv to destination 2 \nAny other key: This text. \n";
 
     DIR* currentdir = opendir(".");
 
-    string destination1 = "/media/storage/traditional games/Images/Landscapes/Fantasy";
-    string destination2 = "/media/storage/traditional games/Images/Landscapes/Future";
+    string destination1;
+    string destination2;
+
+    cout << "Enter two destination directories. ";
+    cin >> destination1;
+    cin >> destination2;
+    //TODO: Check if actual directories. More than two destination.
+
+    cout << "Usage: \n" << help;
 
     while (1){
-        dirent* b = readdir(currentdir);
-        string file = b[0].d_name;
+        dirent* d = readdir(currentdir);
+        string file = d[0].d_name;
         if (file.find(".jpg") != string::npos | file.find(".png") != string::npos) {
 
-            cout << file;
+            cout << file <<  endl;
 
             system((command0 + " '" + file + "'").c_str());
-            cout <<"choices: 1, 2, 3." << "\n";
+            cout << "Command? \n";
             cin >> a;
             switch(a)
             {
                 case 1: system((command1 + " '" + file + "'").c_str());//(combined string) to char, as required by command()
                 case 2: system((command2 + " '" + file + "' '" + destination1 + "'").c_str());
                 case 3: system((command2 + " '" + file + "' '" + destination2 + "'" ).c_str());
-                default: cout << "1: delete \n2:mv1\n3:mv2 \n";
-
+                default: cout << help;
             }
         }
     }
